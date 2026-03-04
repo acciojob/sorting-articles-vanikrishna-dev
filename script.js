e56651eb339c30903051a78793b1d3b1fdce99e4
vanikrishna-dev <vanikrishna02@gmail.com>
2026-03-04 22:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
+
+function removeArticle(name) {
+  let words = name.split(" ");
+
+  if (words[0] === "A" || words[0] === "An" || words[0] === "The") {
+    words.shift();
+  }
+
+  return words.join(" ");
+}
+
+bands.sort(function(a, b) {
+  if (removeArticle(a) > removeArticle(b)) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+const ul = document.getElementById("band");
+
+for (let i = 0; i < bands.length; i++) {
+  let li = document.createElement("li");
+  li.textContent = bands[i];
+  ul.appendChild(li);
+}
